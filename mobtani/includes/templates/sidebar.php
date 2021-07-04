7e9518434d7a2a42a03f45ca2a2f6c873f03d0da
Iman Barati <Imanbarati2017@outlook.com>
2021-07-04 09:43:23 +0430

    index

--- mobtani/includes/templates/sidebar.php
+++ mobtani/includes/templates/sidebar.php
@@ -1,8 +1,53 @@
+ <?php
+//ini_set('max_execution_time', 120);
+//require("twitter.class.php");
 
+// create an instance of the twitter-class
+//$twitter = new twitter();
+?> 
 		<aside class = "col-md-4 col-12">
+    
 			<p>
 				<ul style="background: #ff9999";>
-					نشانی:اصفهان-خیابان جی--پلاک6
+					نشانی:خیابان جی--پلاک6
 </ul>
 			</p>
+<script>
+function myFunction() {
+  var x = "Is the browser online? " + navigator.onLine;
+  document.getElementById("demo").innerHTML = x;
+}
+</script>
+<p id="demo">Help</p>
+<!-- 			<div id="twitter_box">
+<ul id="twitter_update_list"><?php //echo $twitter->latest_tweet('IMANMANATI', 5); ?></ul>
+<div class="tell_followers"><a title="Follow @IMANMANATI" href="http://www.twitter.com/IMANMANATI" rel="external"><strong>@IMANMANATI</strong> <?php //print $twitter->followers_counter('IMANMANATI') ?> followers</a></div>
+</div>
+
+You can also display multiple user feeds in the same box like this:
+
+<div id="twitter_box">
+<ul id="twitter_update_list">
+<!-- <?php
+//$username = "IMANMANATI";
+//$tweetnumber = "10"; // Number of tweets to pull in
+//parse_cache_feed( $username, $tweetnumber );
+?> 
+</ul>
+</div> --><a class="twitter-share-button" 
+  href="https://twitter.com/intent/tweet?text=Hello%20world"
+  data-size="large"><link rel="canonical"
+  href="/web/tweet-button"><link rel="me"
+  href="https://twitter.com/twitterdev"
+>
+Tweet</a>
+<a class="twitter-mention-button"
+  href="https://twitter.com/intent/tweet?screen_name=TwitterDev">
+Tweet to @TwitterDev</a>
+<a class="twitter-timeline" href="https://twitter.com/iran?ref_src=twsrc%5Etfw">Tweets by imanmanati</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script><a href="https://twitter.com/intent/tweet?button_hashtag=LoveTwitter&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #LoveTwitter</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+	<button onclick="myFunction()" >Help</button>
+  
+
+  <iframe src="https://calendar.google.com/calendar/embed?src=baratiiman3%40gmail.com&ctz=Asia%2FTehran" style="border: 0" width="360" height="360" frameborder="0" scrolling="no"></iframe>
 		</aside>
+    
---
+++ test/phpfreechat-master/README.md
@@ -0,0 +1,58 @@
+# Installation quickstart
+
+## Prerequisites
+
+  * Web browser compatible with JQuery (almost all !)
+  * A server with:
+    * php >= 5.3.0 ([Slim framework](https://github.com/codeguy/Slim/blob/master/README.markdown#system-requirements) dependency)
+    * apache server with mod_rewrite and .htaccess enabled (AllowOverride All)
+    * write access to the phpfreechat-2.1.1/server/data/ and phpfreechat-2.1.1/server/log/ folder (777 or write permission for the web server)
+  * No database needed !
+
+## Quick start
+
+Download [phpfreechat-2.1.1.zip](http://www.phpfreechat.net/download) and unzip it in the root folder of your Web server.
+
+JQuery should be included in your html `<head>` before the phpfreechat code:
+```html
+  <script src="/phpfreechat-2.1.1/client/lib/jquery-1.8.2.min.js" type="text/javascript"></script>
+```
+
+Include the phpfreechat plugin in your html `<head>`:
+```html
+  <link rel="stylesheet" type="text/css" href="/phpfreechat-2.1.1/client/themes/default/pfc.min.css" />
+  <script src="/phpfreechat-2.1.1/client/pfc.min.js" type="text/javascript"></script>
+```
+
+Add this piece of HTML in your `<body>` where you want the chat to be displayed:
+```html
+...
+<div id="mychat"><a href="http://www.phpfreechat.net">Creating chat rooms everywhere - phpFreeChat</a></div>
+...
+```
+
+Then add this piece of code just after (it will hook the chat on the page):
+```html
+<script type="text/javascript">
+  $('#mychat').phpfreechat({ serverUrl: '/phpfreechat-2.1.1/server' });
+</script>
+```
+
+## Themes customization
+
+phpfreechat is released with few themes. You can choose which one you want to use:
+
+* `default`
+* `carbon`
+* `gamer`
+* `phpfreechat`
+* `phpfreechat-mini`
+
+To select the theme, you only have to change one line in your html `<head>`. To use the `default` theme:
+```html
+  <link rel="stylesheet" type="text/css" href="/phpfreechat-2.1.1/client/themes/default/pfc.min.css" />
+```
+or this code for `carbon` theme:
+```html
+  <link rel="stylesheet" type="text/css" href="/phpfreechat-2.1.1/client/themes/carbon/pfc.min.css" />
+```
